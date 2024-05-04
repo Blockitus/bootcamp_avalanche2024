7fb963d519423e7f2edafbd92cc8b125d7c8bba7
pymachado <pyuris22@gmail.com>
2024-05-04 12:55:48 -0600

    feat: holders container was added

--- contracts/BERC20.sol
+++ contracts/BERC20.sol
@@ -9,6 +9,7 @@ contract  Blockitus is ERC20, AccessControl {
     bytes32 constant private BURNER = keccak256(abi.encodePacked("BURNER"));
 
     uint256 constant public INITIALSUPPLY = 100_000_000;
+    uint256 public holders;
     address public airdrop;
 
     error ZeroAddressNotAllowed();
@@ -29,5 +30,17 @@ contract  Blockitus is ERC20, AccessControl {
     function burn() onlyRole(BURNER) public {
         _burn(airdrop, 1*10**decimals());
     }
+
+    function _update(address from, address to, uint256 value) internal override virtual {
+        if(balanceOf(to) == 0) {
+            holders += 1;
+        }
+        if(balanceOf(from) == value) {
+            holders -= 1;
+        }
+        super._update(from, to, value);
+    }
+
+
 
 }
